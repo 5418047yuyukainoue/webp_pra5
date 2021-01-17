cb73d731644f3cadc42c85a1a0a8a3dd09202ced
5418047yuyukainoue <65319887+5418047yuyukainoue@users.noreply.github.com>
2021-01-17 22:07:06 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -7,24 +7,23 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
-let insertY = ['the soup kitchen','Disneyland','the White House'];
-let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+let storyText='It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+let insertX=['Willy the Goblin','Big Daddy','Father Christmas'];
+let insertY=['the soup kitchen','Disneyland','the White House'];
+let insertZ=['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
 
 randomize.addEventListener('click', result);
 
 function result() {
-  let newStory = storyText;
+  let newStory=storyText;
 
-  let xItem = randomValueFromArray(insertX);
-  let yItem = randomValueFromArray(insertY);
-  let zItem = randomValueFromArray(insertZ);
+  let xItem=randomValueFromArray(insertX);
+  let yItem=randomValueFromArray(insertY);
+  let zItem=randomValueFromArray(insertZ);
 
-  newStory = newStory.replace(':insertx:',xItem);
-  newStory = newStory.replace(':insertx:',xItem);
-  newStory = newStory.replace(':inserty:',yItem);
-  newStory = newStory.replace(':insertz:',zItem);
+  newStory=newStory.replace(':insertx:',xItem);
+  newStory=newStory.replace(':inserty:',yItem);
+  newStory=newStory.replace(':insertz:',zItem);
 
   if(customName.value !== '') {
     const name = customName.value;
@@ -38,6 +37,6 @@ function result() {
     newStory = newStory.replace('300 pounds',weight);
   }
 
-  story.textContent = newStory;
+  story.textContent =newStory;
   story.style.visibility = 'visible';
 }
